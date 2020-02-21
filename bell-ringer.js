ce5a27899001b52cd0f9d68c01671ed77145bf0e
Chris Angelico <rosuav@gmail.com>
2020-02-21 20:52:55 +1100

    Implement title fixing

--- bell-ringer.js
+++ bell-ringer.js
@@ -1,8 +1,14 @@
 function fix() {
-	//TODO: Fix the tab hover text (probably the document.title)
+	//Fix the document title, which should fix the tab hover text
+	console.log("Checking title", document.title);
+	const m = /^\([0-9]+\) (.*)$/.exec(document.title);
+	if (m) document.title = m[1];
+
 	//TODO: Fix the tab icon
 }
 
-fix();
-//TODO: Also call fix() any time it appears that a notification has been
-//posted. DOM listener??
+fix(); //Do the fixes immediately on startup
+
+//Also call fix() any time it appears that a notification has been
+//posted. To notice this, we look for a change to the <title> node.
+new MutationObserver(fix).observe(document.querySelector("title"), {childList: true});
