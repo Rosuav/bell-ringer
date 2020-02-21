e19aeae2caaab4c1e7ed78456f711f59e8860a89
Chris Angelico <rosuav@gmail.com>
2020-02-21 21:04:16 +1100

    Per TODO, also clean up the tab icon

--- bell-ringer.js
+++ bell-ringer.js
@@ -1,10 +1,18 @@
+let default_image = null;
+
 function fix() {
 	//Fix the document title, which should fix the tab hover text
-	console.log("Checking title", document.title);
+	//console.log("Checking title", document.title);
 	const m = /^\([0-9]+\) (.*)$/.exec(document.title);
 	if (m) document.title = m[1];
 
-	//TODO: Fix the tab icon
+	//Fix the tab icon
+	const icon = document.querySelector("link[rel=icon]");
+	//console.log("Checking icon", icon.href);
+	if (icon.href.includes("notification"))
+		icon.href = default_image || "favicon.ico";
+	else
+		default_image = icon.href; //Presume that anything that doesn't say "notification" should be kept.
 }
 
 fix(); //Do the fixes immediately on startup
